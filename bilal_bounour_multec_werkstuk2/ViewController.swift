7068bbd39361d13e681133dff625b24473543588
Samed Yildirim <samedyildirim@samed-macbook-air.local>
2018-05-13 06:08:16 +0200

    Werkstuk 2

--- bilal_bounour_multec_werkstuk2/ViewController.swift
+++ bilal_bounour_multec_werkstuk2/ViewController.swift
@@ -1,69 +1,100 @@
 //
 //  ViewController.swift
-//  bilal_bounour_multec_werkstuk2
+//  Camil_Zaim_multec_Werkstuk2
 //
-//  Created by Bilal Bounour on 12/05/18.
-//  Copyright © 2018 bounour_bilal. All rights reserved.
+//  Created by Bilal Bounour.
+//  Copyright © 2018 Bilal Bounour. All rights reserved.
 //
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+struct Villo:Decodable {
+    let number: Int
+    let name: String
+    let position: Locatie?
+}
+
+struct Locatie:Decodable {
+    let lat: Double
+    let lng: Double
+}
+
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
+    
+    @IBOutlet var lblnaam: UILabel!
+    @IBOutlet weak var lblTime: UILabel!
     @IBOutlet weak var mapView: MKMapView!
+
+    
+    
+    @IBAction func btnFrancais(_ sender: UIButton) {
+        let namen = Bundle.main.path(forResource: "fr", ofType: "lproj")
+        let FR = Bundle.init(path: namen!)! as Bundle
+        lblnaam.text = FR.localizedString(forKey: "naam", value: nil, table: nil)
+    }
+    
+    @IBAction func btnNederlands(_ sender: UIButton) {
+        let namen = Bundle.main.path(forResource: "nl", ofType: "lproj")
+        let NL = Bundle.init(path: namen!)! as Bundle
+        lblnaam.text = NL.localizedString(forKey: "naam", value: nil, table: nil)
+    }
+    
+    
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let initialLocation = CLLocation(latitude: 50.850346, longitude: 4.351721)
+     
         
-        let regionRadius: CLLocationDistance = 1000
-        func centerMapOnLocation(location: CLLocation) {
-            let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
-                                                                      regionRadius, regionRadius)
-            mapView.setRegion(coordinateRegion, animated: true)
-        }
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
         
-        centerMapOnLocation(location: initialLocation)
+        let url = "https://api.jcdecaux.com/vls/v1/stations?apiKey=6d5071ed0d0b3b68462ad73df43fd9e5479b03d6&contract=Bruxelles-Capitale"
+        let urlObj = URL(string: url)
         
-        mapView.delegate = self
+        URLSession.shared.dataTask(with: urlObj!) {(data, response, error) in
+            
+            do{
+                let Villobxl = try JSONDecoder().decode([Villo].self, from: data!)
+                
+                for villo in Villobxl {
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = CLLocationCoordinate2D(latitude: (villo.position?.lat)!, longitude: (villo.position?.lng)!)
+                    annotation.title = villo.name
+                    annotation.subtitle = villo.name
+                    
+                    self.mapView.addAnnotation(annotation)
+                }
+            } catch{
+                print("Error")
+            }
+            }.resume()
         
-        let villoBank = VilloBank(title: "Anspach",
-                              locationName: "Anspach Shopping",
-                              coordinate: CLLocationCoordinate2D(latitude: 50.850346, longitude: 4.351721))
-        mapView.addAnnotation(villoBank)
-        
-        // Do any additional setup after loading the view, typically from a nib.
+        refreshDate()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations[0]
+        let center = location.coordinate
+        let span = MKCoordinateSpan(latitudeDelta: 0.4, longitudeDelta: 0.4)
+        let region = MKCoordinateRegion(center: center, span: span)
+        
+        mapView.setRegion(region, animated: true)
+        mapView.showsUserLocation = true
     }
-
+    
+    
+    
+    func refreshDate() {
+        let date = Date()
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+        let minutes = calendar.component(.minute, from: date)
+        lblTime.text = "Laatste refresh :\(hour):\(minutes)"
+    }
 }
-
-extension ViewController: MKMapViewDelegate {
-    // 1
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        // 2
-        guard let annotation = annotation as? VilloBank else { return nil }
-        // 3
-        let identifier = "marker"
-        var view: MKMarkerAnnotationView
-        // 4
-        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
-            as? MKMarkerAnnotationView {
-            dequeuedView.annotation = annotation
-            view = dequeuedView
-        } else {
-            // 5
-            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-            view.canShowCallout = true
-            view.calloutOffset = CGPoint(x: -5, y: 5)
-            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-        }
-        return view
-    }
-}
-
